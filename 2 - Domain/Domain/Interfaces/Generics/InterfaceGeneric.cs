09ff5b87e4019039ac6373accb502e75e2400c42
Guilherme de Castro <guilhermecastro1000@gmail.com>
2023-11-21 13:56:37 -0300

    InterfaceGeneric

---
+++ 2 - Domain/Domain/Interfaces/Generics/InterfaceGeneric.cs
@@ -0,0 +1,10 @@
+﻿namespace Domain.Interfaces.Generics;
+
+public interface InterfaceGeneric<T> where T : class
+{
+    Task Add(T Object);
+    Task Update(T Object);
+    Task Delete(T Object);
+    Task<T> GetEntityById(int Id);
+    Task<List<T>> List();
+}
