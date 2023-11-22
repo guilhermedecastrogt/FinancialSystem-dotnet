6dac417c535f15a9b195709693025a8b8752127e
Guilherme de Castro <guilhermecastro1000@gmail.com>
2023-11-22 17:04:31 -0300

    ExpenseRepository

--- Infra/Repository/ExpenseRepository.cs
+++ Infra/Repository/ExpenseRepository.cs
@@ -15,13 +15,23 @@ public class ExpenseRepository : RepositoryGenerics<Expense>, IExpense
         _context = new DbContextOptions<ContextBase>();
     }
     
-    public Task<IList<Expense>> ExpenseUserList(string userEmail)
+    public async Task<IList<Expense>> ExpenseUserList(string userEmail)
     {
-        throw new NotImplementedException();
+        using (var context = new ContextBase(_context))
+        {
+            return await
+                (from s in context.FinancialSystem
+                    join c in context.Category on s.Id equals c.SystemId
+                    join us in context.FinancialSystemUser on s.Id equals us.IdSystem
+                    join d in context.Expense on c.Id equals d.IdCategory
+                    where us.UserEmail.Equals(userEmail) && s.Month == d.Month && s.Year == d.Year
+                    select d)
+                .AsNoTracking().ToListAsync();
+        }
     }
 
-    public Task<IList<Expense>> UnpaidExpenseUserList(string userEmail)
+    public async Task<IList<Expense>> UnpaidExpenseUserList(string userEmail)
     {
         throw new NotImplementedException();
     }
-}
+} 
