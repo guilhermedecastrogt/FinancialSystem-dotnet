d1055419fda6643ceb71560835872c1a0412e34d
Guilherme de Castro <guilhermecastro1000@gmail.com>
2023-11-22 16:56:56 -0300

    Repositorys

--- Infra/Repository/CategoryRepository.cs
+++ Infra/Repository/CategoryRepository.cs
@@ -1,13 +1,30 @@
 ﻿using Domain.Interfaces.ICategory;
 using Entities.Entities;
+using Infra.Configuracao;
 using Infra.Repository.Generics;
+using Microsoft.EntityFrameworkCore;
 
 namespace Infra.Repository;
 
 public class CategoryRepository : RepositoryGenerics<Category>, ICategory
 {
-    public Task<IList<Category>> ListCategoryUser(string userEmail)
+    private readonly DbContextOptions<ContextBase> _context;
+
+    public CategoryRepository()
     {
-        throw new NotImplementedException();
+        _context = new DbContextOptions<ContextBase>();
+    }
+    public async Task<IList<Category>> ListCategoryUser(string userEmail)
+    {
+        using(var context = new ContextBase(_context))
+        {
+            return await
+                (from s in context.FinancialSystem
+                    join c in context.Category on s.Id equals c.SystemId
+                    join us in context.FinancialSystemUser on s.Id equals us.IdSystem
+                    where us.UserEmail.Equals(userEmail) && us.CurrentSystem
+                    select c)
+                .AsNoTracking().ToListAsync();
+        }
     }
 }
--- Infra/Repository/ExpenseRepository.cs
+++ Infra/Repository/ExpenseRepository.cs
@@ -1,11 +1,20 @@
 ﻿using Domain.Interfaces.IExpense;
 using Entities.Entities;
+using Infra.Configuracao;
 using Infra.Repository.Generics;
+using Microsoft.EntityFrameworkCore;
 
 namespace Infra.Repository;
 
 public class ExpenseRepository : RepositoryGenerics<Expense>, IExpense
 {
+    private readonly DbContextOptions<ContextBase> _context;
+
+    public ExpenseRepository()
+    {
+        _context = new DbContextOptions<ContextBase>();
+    }
+    
     public Task<IList<Expense>> ExpenseUserList(string userEmail)
     {
         throw new NotImplementedException();
